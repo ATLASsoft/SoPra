78aa39d9801fbcf1ea70cc7285ef58323874006d
Tobi <tobi_91@onlinehome.de>
2013-06-10 23:33:06 +0200

    TrainTypeDialog verbessert und internationalisiert

--- NodeAnimator/src/de/atlassoft/ui/TrainTypeDialog.java
+++ NodeAnimator/src/de/atlassoft/ui/TrainTypeDialog.java
@@ -11,6 +11,8 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 import de.atlassoft.ui.MainWindow;
+import de.atlassoft.util.I18NService;
+import de.atlassoft.util.I18NSingleton;
 
 /**
  * This class is for creating a new traintype.
@@ -23,42 +25,52 @@ public class TrainTypeDialog {
 	
 	public TrainTypeDialog(Display display){
 		shell = new Shell(display);
-		shell.setText("Zugtyp erstellen");
+		I18NService I18N = I18NSingleton.getInstance();
+		shell.setText(I18N.getMessage("TrainTypeDialog.title"));
 		shell.setSize(450, 200);
-		MainWindow.center(shell);
 		shell.setLayout(new GridLayout(3, false));
 		initUI();
+		MainWindow.center(shell);
 		shell.open();
 	}
 	
 	private void initUI(){
-		new Label(shell, SWT.NONE).setText("Name: ");
+		I18NService I18N = I18NSingleton.getInstance();
+		
+		// First row
+		new Label(shell, SWT.NONE).setText(I18N.getMessage("TrainTypeDialog.labelNameOfSchedule"));
 		Text name = new Text(shell, SWT.BORDER);
-		name.setText("Name des Fahrplans");;
+		name.setText(I18N.getMessage("TrainTypeDialog.textNameOfSchedule"));
+		name.selectAll();
 		new Label(shell, SWT.NONE);
 		
-		new Label (shell, SWT.NONE).setText("Höchstgeschwindigkeit: ");
+		// Second row
+		new Label (shell, SWT.NONE).setText(I18N.getMessage("TrainTypeDialog.label1Speed"));
 		Text geschwindigkeit = new Text(shell, SWT.BORDER);
-		geschwindigkeit.setText("Bitte geben Sie Ganzzahlige Werte ein");
-		new Label(shell, SWT.NONE).setText("km/h");
+		geschwindigkeit.setText(I18N.getMessage("TrainTypeDialog.textSpeed"));
+		geschwindigkeit.selectAll();
+		new Label(shell, SWT.NONE).setText(I18N.getMessage("TrainTypeDialog.label2Speed"));
 		
-		new Label(shell, SWT.NONE).setText("Priorität: ");
+		// Third row
+		new Label(shell, SWT.NONE).setText(I18N.getMessage("TrainTypeDialog.labelPriority"));
 		final String[] prioritaeten = { "1", "2", "3", "4", "5" };
 		Combo combo = new Combo(shell, SWT.DROP_DOWN);
 	    combo.setItems(prioritaeten);
 		new Label(shell, SWT.NONE);
 		
+		// Empty row
 		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
 		
+		// Last row
 		Button speichern = new Button(shell, SWT.PUSH);
-		speichern.setText("Speicher");
+		speichern.setText(I18N.getMessage("TrainTypeDialog.buttonSave"));
 	    Image speichernBild = new Image(null, "img/greencheck.png");
 	    speichern.setImage(speichernBild);
 		new Label(shell, SWT.NONE);
 		Button abbrechen = new Button(shell, SWT.PUSH);
-		abbrechen.setText("Abbrechen");
+		abbrechen.setText(I18N.getMessage("TrainTypeDialog.buttonCancle"));
 	    Image abbrechenBild = new Image(null, "img/redX.png");
 	    abbrechen.setImage(abbrechenBild);
 		shell.pack();
