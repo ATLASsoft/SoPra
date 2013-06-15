5ef6d5cf304850e17b4e8e7a90140b31fc102a88
Silvan <silvan.haeussermann@gmail.com>
2013-06-15 15:17:14 +0200

    - ListDialog fertig implementiert

--- NodeAnimator/src/de/atlassoft/ui/ListDialog.java
+++ NodeAnimator/src/de/atlassoft/ui/ListDialog.java
@@ -3,6 +3,7 @@ package de.atlassoft.ui;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.RowData;
@@ -10,17 +11,16 @@ import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TableItem;
 
+import de.atlassoft.application.ApplicationService;
 import de.atlassoft.util.I18NService;
 import de.atlassoft.util.I18NSingleton;
 import de.atlassoft.util.ImageHelper;
 
 /**
- * The class list dialog is for loading and deleting railsystems.
+ * The class list dialog is for loading and deleting railwaysystems.
  * 
  * @author Silvan Haeussermann
  */
@@ -29,16 +29,17 @@ public class ListDialog {
 	private Shell shell;
 	private Composite listComposite;
 	private I18NService I18N;
-	private DialogMode dialogMode;
+	private List trainSysList;
+	private ApplicationService applicationService;
 	
-	public ListDialog() {
+	public ListDialog(ApplicationService applicationService) {
 		
 		I18N = I18NSingleton.getInstance();
-		this.dialogMode = dialogMode;
+		this.applicationService = applicationService;
 		shell = new Shell(Display.getCurrent(), SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
 		shell.setText(I18N.getMessage("ListDialog.Title"));
 		shell.setImage(ImageHelper.getImage("trainIcon"));
-		shell.setSize(500, 320);
+		shell.setSize(500, 260);
 		GridLayout shellLayout = new GridLayout();
 		shell.setLayout(shellLayout);
 		GridData shellGridData = new GridData();
@@ -51,39 +52,40 @@ public class ListDialog {
 	}
 	
 	private void initUI() {
-		//TODO: implement method
+		
 		listComposite = new Composite(shell, SWT.NONE);
 		GridLayout listGridLayout = new GridLayout();
 		listGridLayout.numColumns = 3;
 		listComposite.setLayout(listGridLayout);
+		GridData listCoompGridData = new GridData();
+		listCoompGridData.verticalAlignment = SWT.FILL;
+		listCoompGridData.horizontalAlignment = SWT.FILL;
+		listCoompGridData.grabExcessHorizontalSpace = true;
+		listCoompGridData.grabExcessVerticalSpace = true;
+		listComposite.setLayoutData(listCoompGridData);
+		
+		Font listFont = new Font(Display.getCurrent(), "Arial", 14, SWT.NONE);
+		
+		trainSysList = new List(listComposite, SWT.BORDER|SWT.V_SCROLL|SWT.SINGLE);
+		trainSysList.setFont(listFont);
 		GridData listGridData = new GridData();
-		listGridData.verticalAlignment = SWT.FILL;
+		listGridData.horizontalSpan = 3;
 		listGridData.horizontalAlignment = SWT.FILL;
-		listGridData.grabExcessHorizontalSpace = true;
+		listGridData.verticalAlignment = SWT.FILL;
 		listGridData.grabExcessVerticalSpace = true;
-		listComposite.setLayoutData(listGridData);
+		trainSysList.setLayoutData(listGridData);
+		//TODO: Mit richtigen Daten füllen
+		trainSysList.add("Streckennetz 1");
+		trainSysList.add("Streckennetz 2");
+		trainSysList.add("Streckennetz 3");
+		trainSysList.add("Streckennetz 4");
+		trainSysList.add("Streckennetz 5");
+		trainSysList.add("Streckennetz 6");
+		trainSysList.add("Streckennetz 7");
+		trainSysList.add("Streckennetz 8");
+		trainSysList.add("Streckennetz 9");
+		trainSysList.add("Streckennetz 10");
 		
-		Table table = new Table(listComposite, SWT.NONE);
-		table.setHeaderVisible(true);
-		table.setLinesVisible(true);
-		table.setToolTipText(I18N.getMessage("ListDialog.TableToolTip"));
-		GridData tableGridData = new GridData();
-		tableGridData.verticalAlignment = SWT.FILL;
-		tableGridData.horizontalAlignment = SWT.FILL;
-		tableGridData.grabExcessHorizontalSpace = true;
-		tableGridData.grabExcessVerticalSpace = true;
-		tableGridData.horizontalSpan = 3;
-		table.setLayoutData(tableGridData);
-		
-		TableColumn column = new TableColumn(table, SWT.LEFT);
-		column.setText("Streckennetz");
-		column.setWidth(200);
-		
-		for (int i=0; i<10; i++){
-			TableItem item = new TableItem(table, SWT.NONE);
-			item.setText("test");
-		}
-		table.redraw();
 		
 		Composite buttonComposite = new Composite(listComposite, SWT.NONE);
 		GridData buttonGridData = new GridData();
@@ -99,12 +101,26 @@ public class ListDialog {
 		loadButton.setImage(ImageHelper.getImage("loadButton"));
 		RowData loadRowData = new RowData(130, 52);
 		loadButton.setLayoutData(loadRowData);
+		loadButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				//TODO: Mit richtigen Daten ausführen
+				applicationService.setActiveRailwaySystem(null);
+			}
+		});
 
 		Button deleteButton = new Button(buttonComposite, SWT.PUSH);
 		deleteButton.setText(I18N.getMessage("ListDialog.Delete"));
 		deleteButton.setImage(ImageHelper.getImage("trashIcon"));
 		RowData deleteRowData = new RowData(130, 52);
 		deleteButton.setLayoutData(deleteRowData);	
+		deleteButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				//TODO: Mit richtigen Daten füllen
+				applicationService.deleteRailwaySystem(null);
+			}
+		});
 		
 		Button cancelButton = new Button(buttonComposite, SWT.PUSH);
 		cancelButton.setText(I18N.getMessage("ListDialog.Cancel"));
@@ -113,11 +129,9 @@ public class ListDialog {
 		cancelButton.setLayoutData(cancelRowData);
 		cancelButton.addSelectionListener(new SelectionAdapter() {
 			@Override
-			public void widgetSelected(SelectionEvent e){
+			public void widgetSelected(SelectionEvent e) {
 				shell.close();
 			}
-		});
-		
-		listComposite.layout();
+		});		
 	}
 }
