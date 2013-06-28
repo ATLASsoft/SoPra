67e97416c1e383a8324d5b4aeadf17075f3ec1c6
Silvan <silvan.haeussermann@gmail.com>
2013-06-28 12:31:23 +0200

    - schedulesAndTrainTypeComposite überarbeitet

--- NodeAnimator/src/de/atlassoft/ui/ScheduleAndTrainTypeComposite.java
+++ NodeAnimator/src/de/atlassoft/ui/ScheduleAndTrainTypeComposite.java
@@ -9,27 +9,56 @@ import de.atlassoft.application.ApplicationService;
 import de.atlassoft.util.I18NService;
 import de.atlassoft.util.I18NSingleton;
 
+/**
+ * This class creates the Tab Folder in which the train types
+ * and the schedules are shown.
+ * 
+ * @author Tobias Ilg, Silvan Haeussermann 
+ *
+ */
 public class ScheduleAndTrainTypeComposite {
 	
 	private Composite scheduleAndTrainTypeComposite;
 	private I18NService I18N;
-	private ApplicationService application;
+	private ApplicationService applicationService;
 	
+	/**
+	 * Constructor for the class ScheduleAndTrainTypeComposite
+	 * 
+	 * @param tabFolder
+	 * 		The tab folder in which the composite lays.
+	 * @param applicationService
+	 * 		The application Service of the application.
+	 */
 	public ScheduleAndTrainTypeComposite(TabFolder tabFolder, ApplicationService applicationService) {
 		I18N = I18NSingleton.getInstance();
-		this.application = applicationService;
+		this.applicationService = applicationService;
 		this.scheduleAndTrainTypeComposite = new Composite (tabFolder, SWT.BORDER);
-		scheduleAndTrainTypeComposite.setLayout(new RowLayout());
-		initScheduleUI();
-		initTrainTypeUI();
+		scheduleAndTrainTypeComposite.setLayout(new RowLayout(SWT.HORIZONTAL));
+		Composite scheduleComposite = new Composite(scheduleAndTrainTypeComposite, SWT.BORDER);
+		Composite trainTypeComposite = new Composite (scheduleAndTrainTypeComposite, SWT.BORDER);
+		initScheduleUI(scheduleComposite);
+		initTrainTypeUI(trainTypeComposite);
 	}
 	
-	private void initScheduleUI(){
-		// TODO: ImplementierenO
+	/**
+	 * Creates the elements for the schedule UI.
+	 * 
+	 * @param
+	 * 		The composite for the elements.
+	 */
+	private void initScheduleUI(Composite mainComposite) {
+		// TODO: Implementieren
 	}
 	
-	private void initTrainTypeUI(){
-		// TODO: ImplementierenO
+	/**
+	 * Creates the elements for the train type UI.
+	 * 
+	 * @param
+	 * 		The composite for the elements.
+	 */
+	private void initTrainTypeUI(Composite mainComposite) {
+		// TODO: Implementieren
 	}
 	
 	/**
@@ -38,7 +67,7 @@ public class ScheduleAndTrainTypeComposite {
 	 * @return
 	 * 		The composite
 	 */
-	public Composite getComposite(){
+	public Composite getComposite() {
 		return scheduleAndTrainTypeComposite;
 	}
 }
