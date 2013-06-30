9ec51c09535cd089370d0a4f280aac858978834f
Tobi <tobi_91@onlinehome.de>
2013-06-30 14:44:22 +0200

    SimulationStatistic implementiert

--- NodeAnimator/src/de/atlassoft/model/SimulationStatistic.java
+++ NodeAnimator/src/de/atlassoft/model/SimulationStatistic.java
@@ -1,47 +1,117 @@
 package de.atlassoft.model;
-//TODO: Klasse implementieren
 public class SimulationStatistic {
 
+	java.util.List<TrainRideStatistic> trainRideStatistic;
+	
 	public SimulationStatistic() {
 		
 	}
 	
-	
-	
 	public void addStatistic(TrainRideStatistic statistic) {
-		
+		trainRideStatistic.add(statistic);
 	}
 	
 	public double getMeanDelay() {
-		return 0.0;
+		double meanDelay;
+		meanDelay = getTotalDelay() / getNumberOfRides();
+		return meanDelay;
 	}
 	
 	public double getTotalDelay() {
-		return 0.0;
+		double totalDelay = 0;
+		for (TrainRideStatistic statistic : trainRideStatistic) {
+			java.util.List<Node> stationList = statistic.getStations();
+			for (Node station : stationList) {
+				totalDelay = totalDelay + statistic.getDelay(station);
+			}
+ 		}
+		return totalDelay;
 	}
 	
 	public double getMeanDelay(TrainType trainType) {
-		return 0.0;
+		double meanDelay = 0;
+		for (TrainRideStatistic statistic : trainRideStatistic) {
+			if (statistic.getScheduleScheme().getTrainType() == trainType) {
+				java.util.List<Node> stationList = statistic.getStations();
+				for (Node station : stationList) {
+					meanDelay = meanDelay + statistic.getDelay(station);
+				}
+			}
+		}
+		meanDelay = meanDelay / getNumberOfRides(trainType);
+		return meanDelay;
 	}
 	
 	public double getMeanDelay(Node station) {
-		return 0.0;
+		double meanDelay = 0.0;
+		int numberOfRides = 0;
+		for (TrainRideStatistic statistic : trainRideStatistic) {
+			java.util.List<Node> stationList = statistic.getStations();
+			for (Node allStation : stationList) {
+				if (allStation == station) {
+					meanDelay = meanDelay + statistic.getDelay(station);
+					numberOfRides++;
+				}
+			}
+ 		}
+		meanDelay = meanDelay / numberOfRides;
+		return meanDelay;
 	}
 	
 	public double getMeanDelay(ScheduleScheme scheduleScheme) {
-		return 0.0;
+		double meanDelay = 0.0;
+		for (TrainRideStatistic statistic : trainRideStatistic) {
+			if (statistic.getScheduleScheme() == scheduleScheme) {
+				java.util.List<Node> stationList = statistic.getStations();
+				for (Node station : stationList) {
+					meanDelay = meanDelay + statistic.getDelay(station);
+				}
+			}
+ 		}
+		meanDelay = meanDelay / getNumberOfRides(scheduleScheme);
+		return meanDelay;
 	}
 	
 	public double getMeanDelay(ScheduleScheme scheduleScheme, Node station) {
-		return 0.0;
+		double meanDelay = 0.0;
+		for (TrainRideStatistic statistic : trainRideStatistic) {
+			if (statistic.getScheduleScheme() == scheduleScheme) {
+				java.util.List<Node> stationList = statistic.getStations();
+				for (Node allStation : stationList) {
+					if (allStation == station) {
+						meanDelay = meanDelay + statistic.getDelay(station);
+					}
+				}
+			}
+ 		}
+		meanDelay = meanDelay / getNumberOfRides(scheduleScheme);
+		return meanDelay;
 	}
 	
 	public int getNumberOfRides() {
-		return 0;
+		int numberOfRides;
+		numberOfRides = trainRideStatistic.size();
+		return numberOfRides;
 	}
 	
 	public int getNumberOfRides(TrainType trainType) {
-		return 0;
+		int numberOfRides = 0;
+		for (TrainRideStatistic statistic : trainRideStatistic){
+			if (statistic.getScheduleScheme().getTrainType() == trainType) {
+				numberOfRides++;
+			}
+		}
+		return numberOfRides;
+	}
+	
+	public int getNumberOfRides(ScheduleScheme scheduleScheme) {
+		int numberOfRides = 0;
+		for (TrainRideStatistic statistic : trainRideStatistic){
+			if (statistic.getScheduleScheme() == scheduleScheme) {
+				numberOfRides++;
+			}
+		}
+		return numberOfRides;
 	}
 	
 }
